45099da704a3b5ba2c0d7a9fbb09f5b8222e8f9c
berrygoeswild <berrgoeswild@gmail.com>
2015-12-14 15:18:31 +0100

    Revert "Aufgabe 2.1 und 2.2 zu teilen"
    This reverts commit 9660a7028d9c51565a93ad1a35ede85619c2635b.

--- Leistungsscript.m
+++
@@ -1,7 +0,0 @@
-Wiederstand = [100, 220, 270, 470, 1200, 8200, 10000, 100000, 68000, 1000000]
-Spannung200W=[0.516, 1.160, 1.547, 2.578, 4.770, 5.285, 5.543, 5.414, 5.414, 5.414]
-Spannung60W=[0, 0.129, 0.129, 0.387, 1.031, 4.254, 4.512 ,4.641, 4.641, 4.641]
-Power200W=(Spannung200W.*Spannung200W)./Wiederstand
-Power60W=(Spannung60W.*Spannung60W)./Wiederstand
-plot(Spannung200W, Power200W)
-plot(Spannung60W, Power60W)
